6344be10aa21d6a9774d55d83117707090f90ceb
LCOW2410 <156823177+LCOW2410@users.noreply.github.com>
2025-02-17 17:38:47 -0500

    Add files via upload

---
+++ src/main/java/org/example/Blog.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+import lombok.ToString;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+public class Blog {
+    private List<BlogPost> posts;       // List of blog posts
+    private List<Person> contributors;  // List of contributors (Persons)
+
+    // Constructor for Blog, which takes lists of posts and contributors
+    public Blog(List<BlogPost> posts, List<Person> contributors) {
+        this.posts = posts;
+        this.contributors = contributors;
+    }
+
+
+}
